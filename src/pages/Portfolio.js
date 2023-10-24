a23d60b34e205c5498c659cd18ebaf10dac79586
Maximilian Royal-Eisenberg <maxroyal.eisenberg@gmail.com>
2023-10-23 22:04:22 -0700

    added portfolio images

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -5,10 +5,13 @@ export default function Portfolio() {
     <div>
       <h1>Portfolio Page</h1>
       <ul>
-        <li><a href="https://jogomez.github.io/MovieDetails/">Movie Geek,</a> a webpage where you can learn more about your favorite movies.</li>
-        <p> <img src="./assets/moviegeek.png" alt="Screenshots of Movie Geek"></img></p>
-        <li><a href="https://polar-temple-53244.herokuapp.com/">Rate My Cow, Giraffe or Shrimp,</a> a blog dedicated to sharing pictures of and commenting on cute animals.</li>
-        <p> <img src="./assets/rate-my-cow-giraffe-shrimp.png" alt="Screenshot of Rate My Cow, Giraffe or Shrimp"></img></p>
+        <li><a href="https://jogomez.github.io/MovieDetails/">Movie Geek</a>, a webpage where you can learn more about your favorite movies.</li>
+          <p> <img src="./assets/moviegeek.png" alt="Screenshots of Movie Geek"></img></p>
+        <li><a href="https://polar-temple-53244.herokuapp.com/">Rate My Cow, Giraffe or Shrimp</a>, a blog dedicated to sharing pictures of and commenting on cute animals.</li>
+          <p> <img src="./assets/rate-my-cow-giraffe-shrimp.png" alt="Screenshot of Rate My Cow, Giraffe or Shrimp"></img></p>
+        <li><a href="https://ancient-ravine-85592.herokuapp.com/">Star Gazers</a>, a group project that utilizes the full MERN stack to deliver an app that lets users explore the night sky via stars, constellations, moon phase calendars and other celestial events.</li>
+          <p> <img src="./assets/main_screen.png" alt="Screenshot the main page of Star Gazers"></img></p>
+          <p> <img src="./assets/moon-phase-oakland-screen.png" alt="Screenshot moon phase page of Star Gazers"></img></p>
       </ul>
       <p>
         This will be colorful!
