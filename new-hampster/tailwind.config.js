86d31c5898d0bc43a82c00efe9f8e6ba0e586d89
Maximilian Royal-Eisenberg <maxroyal.eisenberg@gmail.com>
2023-12-06 23:34:06 -0800

    reinstalled tailwind css

--- new-hampster/tailwind.config.js
+++ new-hampster/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js}"],
+  // content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
